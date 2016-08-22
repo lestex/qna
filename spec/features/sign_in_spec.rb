cdade87c8983401c92251cc664840e0f24ffb1d3
Andrey Larin <lestex@gmail.com>
2016-08-22 23:35:08 +0300

    Adds user sign in spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -8,8 +8,22 @@ feature 'User sign in', %q{
   scenario 'Registered user tries to sign in' do
     User.create!(email: 'user@test.com', password: '123456')
     
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully'
+    expect(current_path).to eq root_path
   end
 
   scenario 'Unregistered user tries to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '123456'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password'
+    expect(current_path).to eq new_user_session_path
   end
 end
