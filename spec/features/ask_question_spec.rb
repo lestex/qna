96242ce635e492fa308ac4e3c173ac6d3d49a85c
Andrey <lestex@gmail.com>
2016-08-30 14:38:43 +0300

    changes ask_question feature spec

--- spec/features/ask_question_spec.rb
+++ spec/features/ask_question_spec.rb
@@ -1,27 +1,40 @@
 require 'rails_helper'
 
-feature 'User can ask question' do
+feature 'Ask a question' do
   given(:user) {create(:user)}
-  context 'when signed in' do
-    scenario 'User can ask a question' do
-      visit root_path
+  context 'authenticated user' do
+    scenario 'asks a valid question' do      
       log_in_user(user)
+
+      visit questions_path
       click_link 'Ask question'
       fill_in 'Title', with: 'Question title'
       fill_in 'Body', with: 'Question body'
-
       click_button 'Create question'
+
       expect(page).to have_content 'Question title'
       expect(page).to have_content 'Question body'
     end
+
+    scenario 'asks an invalid question' do      
+      log_in_user(user)
+
+      visit questions_path
+      click_link 'Ask question'
+      fill_in 'Title', with: 'Question title'
+      click_button 'Create question'
+      
+      expect(page).to have_content "can't be blank"      
+    end
   end
 
-  context 'when not signed in' do
-    scenario 'User redirected to a login form' do
+  context 'unauthenticated user' do
+    scenario 'asks question' do
       visit root_path
       click_link 'Ask question'
 
       expect(page).to have_content 'You need to sign in'
+      expect(page).to have_current_path new_user_session_path
     end
   end
   
