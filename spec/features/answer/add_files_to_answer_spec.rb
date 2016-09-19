8669750d9d44c36cce544693c41f0e9f3d628719
Andrey Larin <lestex@gmail.com>
2016-09-19 21:57:39 +0300

    changes to tests after review

--- spec/features/answer/add_files_to_answer_spec.rb
+++ spec/features/answer/add_files_to_answer_spec.rb
@@ -1,32 +1,26 @@
 require_relative '../features_helper'
 
 feature 'Add files to answer' do
-  given(:user) { create(:user) }
-  given(:question) { create(:question) }
-  given(:answer) { create(:answer, question: question)}
-  given(:attachment) { create(:attachment, attachable: answer) }
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question)}
+  given!(:attachment) { create(:attachment, attachable: answer) }
 
   context 'authenticated user' do
-    before do
+
+    scenario 'adds few attachments when asks a question', js: true do
       log_in_user(question.user)
       visit question_path(question)
       fill_in 'answer_body', with: 'Answer Title'
-    end
-    scenario 'adds one attachment when answers', js: true do
-      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-      click_on 'Answer a question'
-
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-    end
-
-    scenario 'adds few attachments when asks a question', js: true do
       click_link 'Add Attachment'
       inputs = page.all('input[type=file]')
+      puts inputs.count
       inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
       inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
       click_button 'Answer a question'
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
-      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+      save_and_open_page
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb'#, href: '/uploads/attachment/file/3/rails_helper.rb'
     end
   end
   scenario 'deletes an attachment', js: true do
--- spec/features/question/add_files_to_question_spec.rb
+++ spec/features/question/add_files_to_question_spec.rb
@@ -2,34 +2,21 @@ require_relative '../features_helper'
 
 feature 'Add files to question' do
   given!(:user) { create(:user) }
-  given(:question) { create(:question) }
-  given(:attachment) { create(:attachment, attachable: question) }
+  given!(:question) { create(:question) }
+  given!(:attachment) { create(:attachment, attachable: question) }
 
-  context 'authenticated user' do
-    before do
-      log_in_user(user)
-      visit new_question_path
-      fill_in 'Title', with: 'Test Title'
-      fill_in 'Body', with: 'Question text'
-    end
-
-    scenario 'adds attachment when asks a question' do
-      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-      click_on 'Create question'
-
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-    end
-
-    scenario 'adds more attachments when asks a question', js: true do
-      click_link 'Add Attachment'
-      inputs = page.all('input[type=file]')
-      inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
-      inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
-      click_on 'Create question'
-
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
-      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
-    end
+  scenario 'adds more attachments when asks a question', js: true do
+    log_in_user(user)
+    visit new_question_path
+    fill_in 'Title', with: 'Test Title'
+    fill_in 'Body', with: 'Question text'
+    click_link 'Add Attachment'
+    inputs = page.all('input[type=file]')
+    inputs[0].set("#{Rails.root}/spec/spec_helper.rb")
+    inputs[1].set("#{Rails.root}/spec/rails_helper.rb")
+    click_on 'Create question'
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
   end
 
   scenario 'deletes an attachment', js: true do
