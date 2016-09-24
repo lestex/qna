429a95c2874e83b491c9463d34cc4a40fdaa05cd
Andrey Larin <lestex@gmail.com>
2016-09-24 23:13:37 +0300

    changes to specs2

--- spec/features/question/vote_for_question_spec.rb
+++ spec/features/question/vote_for_question_spec.rb
@@ -1,46 +1,10 @@
-require_relative '../features_helper.rb'
+require_relative '../features_helper'
 
 feature 'Vote for question' do
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  context 'authenticated user' do
-    scenario 'votes for a question', js: true do
-      log_in_user(user)
-      visit question_path(question)
-
-      expect(page).to_not have_selector(:link_or_button, 'Cancel vote')
-      click_on 'Vote+'
-      within '.vote-rating-question' do
-        expect(page).to have_content('+1')
-      end
-      expect(page).to_not have_selector(:link_or_button, 'Vote+')
-      expect(page).to_not have_selector(:link_or_button, 'Vote-')
-      expect(page).to have_selector(:link_or_button, 'Cancel vote')
-    end
-
-    scenario 'cancels his vote and votes for another question', js: true do
-      log_in_user(user)
-      visit question_path(question)
-      click_on 'Vote+'
-      click_on 'Cancel Vote'
-      click_on 'Vote-'
-      within '.vote-rating-question' do
-        expect(page).to have_content '-1'
-      end
-    end
-  end
-
-  scenario 'cannot vote for the question hi owns', js: true do 
-    log_in_user(question.user)
-    visit question_path(question)
-
-    expect(page).to_not have_selector(:link_or_button, 'Vote+')
-    expect(page).to_not have_selector(:link_or_button, 'Vote-')
-    expect(page).to_not have_selector(:link_or_button, 'Cancel vote')
-  end
-  
-  scenario 'unauthenticated user cannot vote', js: true  do
+  scenario 'Unauthenticated user cannot vote', js: true  do
     visit question_path(question)
 
     expect(page).to_not have_selector(:link_or_button, 'Vote+')
@@ -48,4 +12,43 @@ feature 'Vote for question' do
     expect(page).to_not have_selector(:link_or_button, 'Cancel Vote')
   end
 
- end
+  context 'Authenticated user' do
+    scenario 'cannot vote for question hi owns', js: true do
+      log_in_user(question.user)
+      visit question_path(question)
+
+      expect(page).to_not have_selector(:link_or_button, 'Vote+')
+      expect(page).to_not have_selector(:link_or_button, 'Vote-')
+      expect(page).to_not have_selector(:link_or_button, 'Cancel Vote')
+    end
+
+    scenario 'votes for a question', js: true do
+      log_in_user(user)
+      visit question_path(question)
+
+      expect(page).to_not have_selector(:link_or_button, 'Cancel Vote')
+      click_on 'Vote+'
+      sleep(2)
+      within '.vote-rating-question' do
+        expect(page).to have_content '+1'
+      end
+      save_and_open_page
+      expect(page).to_not have_selector(:link_or_button, 'Vote+')
+      expect(page).to_not have_selector(:link_or_button, 'Vote-')
+      expect(page).to have_selector(:link_or_button, 'Cancel Vote')
+    end
+
+    scenario 'cancels vote and votes for another question', js: true do
+      log_in_user(user)
+      visit question_path(question)
+
+      click_on 'Vote+'
+      save_and_open_page
+      click_on 'Cancel Vote'
+      click_on 'Vote-'
+      within '.vote-rating-question' do
+        expect(page).to have_content '-1'
+      end
+    end
+  end
+end
