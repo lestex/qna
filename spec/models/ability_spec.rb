6bbbffa609343d6a7b9a5b74201771508c84567a
Andrey <lestex@gmail.com>
2016-10-20 10:51:07 +0300

    adds ability and tests

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Ability do
+  subject(:ability) { Ability.new(user) }
+
+  describe 'for guest' do
+    let(:user) { nil }
+
+    it { should be_able_to :read, Question }
+    it { should be_able_to :read, Answer }
+    it { should be_able_to :read, Comment }
+    it { should be_able_to :read, Attachment }
+    it { should be_able_to :read, Vote }
+
+    it { should_not be_able_to :manage, :all }
+  end
+
+  describe 'for admin' do
+    let(:user) { create(:user, admin: true) }
+    it { should be_able_to :manage, :all}
+  end
+
+  describe 'for user' do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+
+    it { should_not be_able_to :manage, :all }
+    it { should be_able_to :read, :all }
+
+    context 'create' do
+      it { should be_able_to :create, Question }
+      it { should be_able_to :create, Answer }
+      it { should be_able_to :create, Comment }
+    end
+    
+  end
+end
