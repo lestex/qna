7148801b2c5f5d91d2e0a0b359c354fb22cf3396
Andrey Larin <lestex@gmail.com>
2016-10-26 00:51:08 +0300

    adds shared examples for API commentable and attachable 2

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -49,22 +49,7 @@ describe 'Answer API' do
       end
 
       it_behaves_like 'API commentable'
-
-      context 'attachments' do
-        it 'included in the answer' do
-          expect(response.body).to have_json_path('attachments')
-        end
-
-        it 'includes the url' do
-          expect(response.body).to include_json((Rails.application.config.hostname_url + attachment.file.url).to_json).at_path('attachments/0/file_url')
-        end
-
-        %w(id created_at updated_at).each do |attr|
-          it "contains the #{attr}" do
-            expect(response.body).to be_json_eql(attachment.send(attr.to_sym).to_json).at_path("attachments/0/#{attr}")
-          end
-        end
-      end
+      it_behaves_like 'API attachable'
     end
   end
 
