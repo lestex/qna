276b3a526a4bf0d184bf57d72ad419b620e06c71
Andrey Larin <lestex@gmail.com>
2016-11-24 01:18:46 +0300

    adds capistrano configs

--- Gemfile
+++ Gemfile
@@ -48,7 +48,10 @@ group :development do
   gem 'listen', '~> 3.0.5'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  # gem 'capistrano-rails', group: :development
+  gem 'capistrano', require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano-rails', require: false
+  gem 'capistrano-rvm', require: false
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,73 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'qna'
+set :repo_url, 'git@github.com:lestex/qna.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/qna'
+set :deploy_user, 'deployer'
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
+
+namespace :private_pub do
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'Stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'Restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
